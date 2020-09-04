7115ffe15787c63a161db09b4cc855047c80fabc
OrlandoRocks <mr.landodlp@gmail.com>
2020-09-03 22:41:28 -0600

    sucursales fix

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,7 +1,7 @@
 class Branch < ApplicationRecord
   belongs_to :company
   belongs_to :user , :class_name => 'User' , foreign_key: 'manager_id'
-  belongs_to :state , :class_name => 'State' , foreign_key: 'state_id'
+  # belongs_to :state , :class_name => 'State' , foreign_key: 'state_id'
   belongs_to :city , :class_name => 'City' , foreign_key: 'city_id'
 
   has_one :state, through: :city
