190845074c1faf4423364de2e2416650d71ad2bc
OrlandoRocks <mr.landodlp@gmail.com>
2020-10-06 14:31:40 -0600

    items.js reestructura y venta de remolques jalando

--- app/assets/javascripts/angular/controllers/item.js
+++ app/assets/javascripts/angular/controllers/item.js
@@ -4,20 +4,6 @@
 app.controller('itemController',["$scope", "ModalService", "$http", function($scope, ModalService, $http) {
 
 
-    // $scope.get_companies_services_json =  function(id) {
-    //     $http.get('/admin/companies_services/show_by_company/'+id+'.json')
-    //         .success(function(data, status, headers, config) {
-    //             $scope.company_data = data;
-    //         })
-    //         .error(function(error, status, headers, config) {
-    //             console.log(status);
-    //             console.log("Error occured");
-    //         });
-    //
-    // };
-
-
-    // $scope.get_companies_services_json($scope.company);
 
     $('.datepicker').datepicker({
         format: 'dd/mm/yyyy',
@@ -46,6 +32,55 @@ app.controller('itemController',["$scope", "ModalService", "$http", function($sc
         }
     };
 
+    
+
+    $scope.show = function() {
+
+        console.log('Show');
+        console.log($scope.item);
+        ModalService.showModal({
+            templateUrl: 'modal_venta.html',
+            controller: "ModalVentaController as modal",
+            inputs:{
+                item: $scope.item,
+                fiscal_vouchers: $scope.fiscal_vouchers
+            }
+        }).then(function(modal) {
+            modal.element.modal();
+            modal.close.then(function(sale_detail) {
+                $scope.showConfirm(sale_detail);
+                // $scope.save_sale_detail(sale_detail);
+                // $scope.amount_paid += parseInt(paid);
+                // $scope.amount_owed -= paid;
+                // $scope.get_sale_details_json($scope.sale);
+            });
+        });
+    };
+
+
+    $scope.get_item_json =  function(id) {
+
+        $http({
+            method: 'GET',
+            url: '/items/'+id+'.json'
+        }).then(function successCallback(response) {
+
+            if (response.data != null) {
+                $scope.item = response.data;
+                $scope.branch = $scope.item.branch_id;
+                $scope.trailer = $scope.item.trailer_id;
+                $scope.get_department();
+                $scope.get_subcategories();
+                $scope.get_trailer($scope.item.trailer_id);
+                $scope.get_fiscal_vouchers();
+            }
+        }, function errorCallback(response) {
+            console.log("Algo valio shit!");
+        });
+
+    };
+
+
     $scope.get_orders = function(id){
         $http({
             method: 'GET',
@@ -64,104 +99,6 @@ app.controller('itemController',["$scope", "ModalService", "$http", function($sc
             console.log("Algo valio shit!");
         });
     };
-
-    $scope.get_trailers = function(){
-        $http({
-            method: 'GET',
-            url: '/trailers.json'
-        }).then(function successCallback(response) {
-            $scope.trailers = response.data;
-        }, function errorCallback(response) {
-            console.log("Algo valio shit!");
-        });
-    };
-
-
-    $scope.showConfirm = function(ev) {
-
-        // Appending dialog to document.body to cover sidenav in docs app
-        // var confirm = $mdDialog.confirm()
-        //     .title('¿Estas Seguro de querer Guardar el Abono?')
-        //     .ariaLabel('Lucky day')
-        //     .targetEvent(ev)
-        //     .ok('Simon')
-        //     .cancel('Nelson');
-        //
-        // $mdDialog.show(confirm).then(function() {
-
-            $scope.save_sale_detail(ev);
-        //     $scope.status = 'You decided to get rid of your debt.';
-        // }, function() {
-        //     $scope.status = 'You decided to keep your debt.';
-        // });
-    };
-
-
-    $scope.get_item_json =  function(id) {
-
-
-
-        $http({
-            method: 'GET',
-            url: '/items/'+id+'.json'
-        }).then(function successCallback(response) {
-
-            if (response.data != null) {
-                $scope.item = response.data;
-                $scope.branch = $scope.item.branch_id;
-                $scope.trailer = $scope.item.trailer_id;
-                $scope.get_department();
-                $scope.get_subcategories();
-                $scope.get_trailer($scope.item.trailer_id);
-            }
-        }, function errorCallback(response) {
-            console.log("Algo valio shit!");
-        });
-
-    };
-
-
-    $scope.save_sale_detail = function(sale_detail){
-
-        $http({
-            url: '/sale_details',
-            method: 'POST',
-            data: {sale_detail: sale_detail}
-        }).then(function successCallback(response) {
-
-            $scope.amount_paid += parseInt(sale_detail.payment);
-            $scope.amount_owed -= parseInt(sale_detail.payment);
-            $scope.get_sale_details_json($scope.sale);
-
-        }, function errorCallback(response) {
-            console.log("algo valio shit!! :(");
-        });
-    };
-
-
-
-    $scope.show = function() {
-
-        console.log('Show');
-        console.log($scope.item);
-        ModalService.showModal({
-            templateUrl: 'modal_venta.html',
-            controller: "ModalVentaController as modal",
-            inputs:{
-                item: $scope.item,
-            }
-        }).then(function(modal) {
-            modal.element.modal();
-            modal.close.then(function(sale_detail) {
-                $scope.showConfirm(sale_detail);
-                // $scope.save_sale_detail(sale_detail);
-                // $scope.amount_paid += parseInt(paid);
-                // $scope.amount_owed -= paid;
-                // $scope.get_sale_details_json($scope.sale);
-            });
-        });
-    };
-
 
 
     $scope.get_department = function(){
@@ -186,40 +123,60 @@ app.controller('itemController',["$scope", "ModalService", "$http", function($sc
         });
     };
 
-    $scope.delete_item = function(id){
-        swal({
-            title: 'Eliminar',
-            text: '¿Estas seguro de eliminar este artículo?',
-            type: 'question',
-            showCancelButton: true
-        }).then(function (isConfirm) {
-            if (isConfirm) {
-                $http({
-                    url: '/items/' + id + '.json',
-                    method: 'DELETE'
-                }).then(function (response) {
-                    if (response.data) {
-                        swal({
-                            title: 'Eliminado',
-                            text: 'El artículo ha sido eliminado',
-                            type: 'success',
-                            showCancelButton: false
-                        }).then(function (isConfirm) {
-                            if (isConfirm) {
-                                location.reload();
-                            }
-
-                        }, function (iSConfirm) {
-
-                        });
-                    }
-                });
-            }
-
-        }, function (iSConfirm) {
-
+    $scope.get_trailers = function(){
+        $http({
+            method: 'GET',
+            url: '/trailers.json'
+        }).then(function successCallback(response) {
+            $scope.trailers = response.data;
+        }, function errorCallback(response) {
+            console.log("Algo valio shit!");
         });
     };
+
+    $scope.get_fiscal_vouchers = function(){
+        $http({
+            method: 'GET',
+            url: '/fiscal_vouchers.json'
+        }).then(function successCallback(response) {
+            $scope.fiscal_vouchers = response.data;
+        }, function errorCallback(response) {
+            console.log("Algo valio shit!");
+        });
+    };
+
+
+
+    $scope.get_branch_user = function(user){
+        $http({
+            url: '/users/'+ user + '.json',
+            method: 'GET'
+        }).then(function (response) {
+            if (response.data != null) {
+
+                $scope.branch_name = response.data.branch.name;
+                $scope.department_name = response.data.department.name;
+                $scope.department_id = response.data.department_id;
+            }
+        });
+    };
+
+    $scope.get_trailer = function(trailer){
+        $http({
+            url: '/trailers/'+ trailer + '.json',
+            method: 'GET'
+        }).then(function (response) {
+            if (response.data != null) {
+                $scope.model_name = response.data.model;
+                $scope.sub_category_name = response.data.sub_category.name;
+                $scope.category_name = response.data.category.name;
+            }
+
+        });
+    }
+
+
+
     $scope.delete_order = function(id){
         swal({
             title: 'Eliminar',
@@ -255,59 +212,74 @@ app.controller('itemController',["$scope", "ModalService", "$http", function($sc
         });
     };
 
-    $scope.get_branch_user = function(user){
-        $http({
-            url: '/users/'+ user + '.json',
-            method: 'GET'
-        }).then(function (response) {
-            if (response.data != null) {
 
-                $scope.branch_name = response.data.branch.name;
-                $scope.department_name = response.data.department.name;
-                $scope.department_id = response.data.department_id;
+    $scope.delete_item = function(id){
+        swal({
+            title: 'Eliminar',
+            text: '¿Estas seguro de eliminar este artículo?',
+            type: 'question',
+            showCancelButton: true
+        }).then(function (isConfirm) {
+            if (isConfirm) {
+                $http({
+                    url: '/items/' + id + '.json',
+                    method: 'DELETE'
+                }).then(function (response) {
+                    if (response.data) {
+                        swal({
+                            title: 'Eliminado',
+                            text: 'El artículo ha sido eliminado',
+                            type: 'success',
+                            showCancelButton: false
+                        }).then(function (isConfirm) {
+                            if (isConfirm) {
+                                location.reload();
+                            }
+
+                        }, function (iSConfirm) {
+
+                        });
+                    }
+                });
             }
+
+        }, function (iSConfirm) {
+
         });
     };
 
-    $scope.get_trailer = function(trailer){
-        $http({
-            url: '/trailers/'+ trailer + '.json',
-            method: 'GET'
-        }).then(function (response) {
-            if (response.data != null) {
-                $scope.model_name = response.data.model;
-                $scope.sub_category_name = response.data.sub_category.name;
-                $scope.category_name = response.data.category.name;
-            }
 
-        });
-    }
 
 }]);
 
 
 
 
-app.controller('ModalVentaController', ['$scope','close' ,'Upload','$http', 'item', '$timeout',function($scope, close, Upload, $http, item, $timeout) {
+app.controller('ModalVentaController', ['$scope','close' ,'Upload','$http', 'item', '', '$timeout',function($scope, close, Upload, $http, item, $timeout) {
 
 
     $scope.open = function($event) {
         $event.preventDefault();
         $event.stopPropagation();
-        console.log("Item Loco");
-        console.log($scope.item);
         $scope.opened = true;
     };
 
 
 
+
+
+
     $scope.close = function() {
-        close(sale_detail, 500); // close, but give 500ms for bootstrap to animate
+        close('', 500); // close, but give 500ms for bootstrap to animate
     };
 
     $scope.signature = "";
 
     $scope.item = item;
+    $scope.payment_types = {1: {id:1, type:'Cash'},
+                            2: {id:2, type:'Deposito'},
+                            3: {id:3, type:'Transferencia'}
+                            };
 
 
     $scope.uploadSell = function() {
@@ -332,9 +304,9 @@ app.controller('ModalVentaController', ['$scope','close' ,'Upload','$http', 'ite
                     }
                 }
             });
-            file.upload.then(function (response) {
+            $scope.item.image.upload.then(function (response) {
                 $timeout(function () {
-                    file.result = response.data;
+                    $scope.item.image.result = response.data;
                     $scope.close("Cancel");
                 });
             }, function (response) {
