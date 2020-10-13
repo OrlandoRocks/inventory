3c95f1fc02a6086920a67a6aa745f79a3836fd5c
Alejandro Rodriguez Gonzalez <alexrdgz03@gmail.com>
2020-10-13 11:48:02 -0600

    Se terminan permisos

--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -1,24 +1,16 @@
-
 #TODO: Cuando se creen los etatus quietar los artículos que esten en baja para los perfiles Director, Gerente y Coordinador
 
 class ItemPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       if @user.god? or @user.admin?
-        if @user.current_company.equal?(0)
-          scope.all
-        end
+        scope.all
       elsif @user.admin_branch?
         scope.where('items.status_item_id = 2')
       elsif @user.user_employee?
         scope.where(status_item_id: 2)
       else
         scope.not
-      end
-    end
-    def orders
-      if @user.god? or @user.admin?
-
       end
     end
   end
