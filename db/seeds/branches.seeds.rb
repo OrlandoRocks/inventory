a86878694ea4e685b488c8565ba05fda27dde863
OrlandoRocks <mr.landodlp@gmail.com>
2021-01-29 12:43:50 -0700

    branch

--- db/seeds/branches.seeds.rb
+++ db/seeds/branches.seeds.rb
@@ -10,11 +10,11 @@
   # Content.
 
   #2
-  Branch.create(name: 'Cuauhtémoc', description: '' , state_id: State.where(name:"Chihuahua").first.id, city_id: 200 )
+  Branch.create(name: 'Cuauhtemoc', description: '' , state_id: State.where(name:"Chihuahua").first.id, city_id: 200 )
   Branch.create(name: 'Alchichica', description: '' , state_id: State.where(name:"Puebla").first.id, city_id: 1646 )
   Branch.create(name: 'Arteaga', description: '', state_id: State.where(name:"Coahuila").first.id, city_id: 33 )
   Branch.create(name: 'Monclova', description: '', state_id: State.where(name:"Coahuila").first.id, city_id: 42 )
-  Branch.create(name: 'Cuerámaro', description: '', state_id: State.where(name:"Guanajuato").first.id, city_id: 331)
+  Branch.create(name: 'Cueramaro', description: '', state_id: State.where(name:"Guanajuato").first.id, city_id: 331)
   Branch.create(name: 'Irapuato', description: '' , state_id: State.where(name:"Guanajuato").first.id, city_id: 327 )
   Branch.create(name: 'CDMX', description: '', state_id: State.where(name:"Mexico").first.id, city_id: 739 )
 
