db9d573d8714ef7dcd82df41fbd9900cfd055da9
OrlandoRocks <mr.landodlp@gmail.com>
2021-01-28 23:14:27 -0700

    branch seed fix

--- db/seeds/branches.seeds.rb
+++ db/seeds/branches.seeds.rb
@@ -10,12 +10,12 @@ after :cities do
   # Content.
 
   #2
-  Branch.create(name: 'Cuauhtémoc', description: '' ,state_i: State.where(name:"Chihuahua").first.id, city_id: City.where(name:"Cuauhtémoc").first.id )
-  Branch.create(name: 'Alchichica', description: '' ,state_i: State.where(name:"Puebla").first.id, city_id: City.where(name:"Tepeyahualco").first.id )
-  Branch.create(name: 'Arteaga', description: ''       ,state_i: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Saltillo").first.id )
-  Branch.create(name: 'Monclova', description: ''     ,state_i: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Monclova").first.id )
-  Branch.create(name: 'Cuerámaro', description: ''   ,state_i: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Cuerámaro").first.id )
-  Branch.create(name: 'Irapuato', description: ''     ,state_i: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Irapuato").first.id )
-  Branch.create(name: 'CDMX', description: ''             ,state_i: State.where(name:"México").first.id, city_id: City.where(name:"Tultitlán").first.id )
+  Branch.create(name: 'Cuauhtémoc', description: '' ,state_id: State.where(name:"Chihuahua").first.id, city_id: City.where(name:"Cuauhtémoc").first.id )
+  Branch.create(name: 'Alchichica', description: '' ,state_id: State.where(name:"Puebla").first.id, city_id: City.where(name:"Tepeyahualco").first.id )
+  Branch.create(name: 'Arteaga', description: ''       ,state_id: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Saltillo").first.id )
+  Branch.create(name: 'Monclova', description: ''     ,state_id: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Monclova").first.id )
+  Branch.create(name: 'Cuerámaro', description: ''   ,state_id: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Cuerámaro").first.id )
+  Branch.create(name: 'Irapuato', description: ''     ,state_id: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Irapuato").first.id )
+  Branch.create(name: 'CDMX', description: ''             ,state_id: State.where(name:"México").first.id, city_id: City.where(name:"Tultitlán").first.id )
 
 end
