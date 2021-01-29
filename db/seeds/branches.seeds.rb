8ddfdd8e2abc1599234310da48ed0e3340c4c02a
OrlandoRocks <mr.landodlp@gmail.com>
2021-01-28 23:10:32 -0700

    seeds devise

--- db/seeds/branches.seeds.rb
+++ db/seeds/branches.seeds.rb
@@ -10,9 +10,12 @@ after :cities do
   # Content.
 
   #2
-  Branch.create(name: 'Alchichica, Puebla', status: 'true',category_id: 1 )
-  Branch.create(name: 'Saltillo, Coahuila', description: 'CABALLOS', status: 'true',category_id: 1)
-  Branch.create(name: 'Saltillo, Coahuila', description: 'ABIERTOS', status: 'true',category_id: 1 )
-  Branch.create(name: 'Irapuato, Guananjuato', description: 'ABIERTOS', status: 'true',category_id: 1 )
+  Branch.create(name: 'Cuauhtémoc', description: '', status: 'true',state_i: State.where(name:"Chihuahua").first.id, city_id: City.where(name:"Cuauhtémoc").first.id )
+  Branch.create(name: 'Alchichica', description: '', status: 'true',state_i: State.where(name:"Puebla").first.id, city_id: City.where(name:"Tepeyahualco").first.id )
+  Branch.create(name: 'Arteaga', description: '', status: 'true'   ,state_i: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Saltillo").first.id )
+  Branch.create(name: 'Monclova', description: '', status: 'true'  ,state_i: State.where(name:"Coahuila").first.id, city_id: City.where(name:"Monclova").first.id )
+  Branch.create(name: 'Cuerámaro', description: '', status: 'true' ,state_i: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Cuerámaro").first.id )
+  Branch.create(name: 'Irapuato', description: '', status: 'true'  ,state_i: State.where(name:"Guanajuato").first.id, city_id: City.where(name:"Irapuato").first.id )
+  Branch.create(name: 'CDMX', description: '', status: 'true'      ,state_i: State.where(name:"México").first.id, city_id: City.where(name:"Tultitlán").first.id )
 
 end
--- db/seeds/devise_user.seeds.rb
+++ db/seeds/devise_user.seeds.rb
@@ -4,56 +4,112 @@
 # User.where(email: 'admin_sucursal@example.com').update_all email: 'gerente@example.com'
 # User.where(email: 'admin_departamento@example.com').update_all email: 'coordinador@example.com'
 
-after :roles do
-  puts '==> Creating the \'god user\'...'
+after :departments do
+  after :roles do
+    puts '==> Creating the \'god user\'...'
 
 # Deletes all existing records.
-  User.delete_all
+    User.delete_all
 
 # Restarts ids to 1.
-  ActiveRecord::Base.connection.reset_pk_sequence!('users')
+    ActiveRecord::Base.connection.reset_pk_sequence!('users')
 
-  User.create(email: 'admin@agroplanet.com', username: 'admin', first_name: 'Admin', last_name: 'Agro',
-              maiden_name: 'Planet', role_id: Role.find_by_key('admin').id, password: 'agroplanet',
-              confirmed_at: Time.now, sign_in_count: 0, employee_number: 77777)
+    User.create(email: 'god@example.com', username: 'divinity', first_name: 'God', last_name: 'System',
+                maiden_name: 'User', role_id: Role.find_by_key('god').id, password: 'inventory',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 12345, department_id: Department.where(name: 'Oficina Central').first.id)
 
-# Content.C
-  User.create(email: 'god@example.com', username: 'divinity', first_name: 'God', last_name: 'System',
-              maiden_name: 'User', role_id: Role.find_by_key('god').id, password: 'inventory',
-              confirmed_at: Time.now, sign_in_count: 0, employee_number: 12345)
-
-  User.create(email: 'alexrdgz03@gmail.com', username: 'alex', first_name: 'Alejandro', last_name: 'Rodriguez',
-              maiden_name: 'UserAlex', role_id: Role.find_by_key('admin_sucursal').id, password: 'inventory',
-              confirmed_at: Time.now, sign_in_count: 0, employee_number: 123)
-
-  User.create(email: 'ovalencia@uach.mx', username: 'orlando', first_name: 'Orlando', last_name: 'Valencia',
-              maiden_name: 'UserOrlando', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'inventory',
-              confirmed_at: Time.now, sign_in_count: 0, employee_number: 567, current_company: 1)
-
-  User.create(email: 'javitorres@uach.mx', username: 'javi', first_name: 'Javier', last_name: 'Torres',
-              maiden_name: 'userJavi', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'inventory',
-              confirmed_at: Time.now, sign_in_count: 0, employee_number: 1245, current_company: 1)
-
-  # User.create(email: 'admin@example.com', username: 'admin', first_name: 'Admin', last_name: 'Admin',
-  #             maiden_name: 'Admin', role_id: Role.find_by_key('admin').id, password: 'password',
-  #             confirmed_at: Time.now, sign_in_count: 0)
-  #
-  # User.create(email: 'director@example.com', username: 'admin', first_name: 'Admin Empresa',
-  #             last_name: 'Admin Empresa', maiden_name: 'Admin Empresa', role_id: Role.find_by_key('admin_empresa').id,
-  #             password: 'password', confirmed_at: Time.now, sign_in_count: 0)
-  #
-  # User.create(email: 'gerente@example.com', username: 'admin_sucursal', first_name: 'Admin Sucursal',
-  #             last_name: 'Admin Sucursal', maiden_name: 'Admin Sucursal', role_id: Role.find_by_key('admin_sucursal').id,
-  #             password: 'password', confirmed_at: Time.now, sign_in_count: 0)
-  #
-  # User.create(email: 'coordinador@example.com', username: 'admin_departamento', first_name: 'Admin Departamento',
-  #             last_name: 'Admin Departamento', maiden_name: 'Admin Departamento',
-  #             role_id: Role.find_by_key('admin_departamento').id, password: 'password', confirmed_at: Time.now,
-  #             sign_in_count: 0)
+    User.create(email: 'peter.loewen@agroplanet.com', username: 'Admin Peter', first_name: 'Pedro', last_name: 'Loewen',
+                maiden_name: '', role_id: Role.find_by_key('gerente').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 0000, department_id: Department.where(name: 'Oficina Central').first.id)
+    Branch.where(name: 'Cuauhtémoc').first.update(manager_id: User.last.id)
+    Department.where(name: 'Oficina Central').first.update(manager_id: User.last.id)
 
 
-  Company.create(name: 'AgroTrailers Planet', description: 'AgroTrailers Planet',
-                 address: 'Cambiar Direccion', phone: 'Cambiar Telefono',
-                 user_id: 1)
+    User.create(email: 'cesar.guerrero@agroplanet.com', username: 'Vendedor Cesar', first_name: 'Cesar', last_name: 'Guerrero',
+                maiden_name: '', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 0101, department_id: Department.where(name: 'Oficina Central').first.id)
 
+
+    User.create(email: 'isaac.gutierrez@agroplanet.com', username: 'Vendedor Isaac', first_name: 'Isaac', last_name: 'Gutierrez',
+                maiden_name: '', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 0102, department_id: Department.where(name: 'Oficina Central').first.id)
+
+
+    User.create(email: 'francisco.orozco@agroplanet.com', username: 'Vendedor Francisco', first_name: 'Francisco', last_name: 'Orozco',
+                maiden_name: '', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 0103, department_id: Department.where(name: 'Oficina Central').first.id)
+
+
+
+    User.create(email: 'hugo.reyes@agroplanet.com', username: 'Gerente Arteaga', first_name: 'Hugo', last_name: 'Reyes',
+                maiden_name: '', role_id: Role.find_by_key('admin_sucursal').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 2000, department_id: Department.where(name: 'Arteaga').first.id)
+    Branch.where(name: 'Arteaga').first.update(manager_id: User.last.id)
+    Department.where(name: 'Arteaga').first.update(manager_id: User.last.id)
+
+
+    User.create(email: 'oscar.guevara@agroplanet.com', username: 'Gerente Arteaga', first_name: 'Óscar', last_name: 'Guevara',
+                maiden_name: 'Rangel', role_id: Role.find_by_key('admin_sucursal').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 3000, department_id: Department.where(name: 'Irapuato').first.id)
+    Branch.where(name: 'Irapuato').first.update(manager_id: User.last.id)
+    Department.where(name: 'Irapuato').first.update(manager_id: User.last.id)
+
+
+
+    User.create(email: 'daniel.ramirez@agroplanet.com', username: 'Vendedor Daniel', first_name: 'Daniel', last_name: 'Ramirez',
+                maiden_name: 'Ruiz', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 3101, department_id: Department.where(name: 'Irapuato').first.id)
+
+    User.create(email: 'lucero.cortez@agroplanet.com', username: 'Vendedor Daniel', first_name: 'Lucero', last_name: 'Cortez',
+                maiden_name: 'Hernandez', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 3102, department_id: Department.where(name: 'Irapuato').first.id)
+
+
+
+
+
+    User.create(email: 'raymundo.leon@agroplanet.com', username: 'Gerente Irapuato', first_name: 'Raymundo', last_name: 'Leon',
+                maiden_name: 'Valenzuela', role_id: Role.find_by_key('admin_sucursal').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 4000, department_id: Department.where(name: 'Cuerámaro').first.id)
+
+    Branch.where(name: 'Cuerámaro').first.update(manager_id: User.last.id)
+    Department.where(name: 'Cuerámaro').first.update(manager_id: User.last.id)
+
+
+    User.create(email: 'maria.elena.guevara@agroplanet.com', username: 'Vendedor Maria Elena', first_name: 'Maria Elena', last_name: 'Guevara',
+                maiden_name: 'Rivera', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 3101, department_id: Department.where(name: 'Cuerámaro').first.id)
+
+
+    User.create(email: 'eduardo.ramos@agroplanet.com', username: 'Vendedor Marcos Eduardos', first_name: 'Marcos Eduardo', last_name: 'Ramos',
+                maiden_name: 'Gonzalez', role_id: Role.find_by_key('empleado_sin_acceso').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 3101, department_id: Department.where(name: 'Cuerámaro').first.id)
+
+
+
+
+    User.create(email: 'eduardo.turner@agroplanet.com', username: 'Gerente Manclova', first_name: 'Eduardo', last_name: 'Turner',
+                maiden_name: 'López', role_id: Role.find_by_key('admin_sucursal').id, password: 'agroplanet',
+                confirmed_at: Time.now, sign_in_count: 0, employee_number: 5000, department_id: Department.where(name: 'Monclova').first.id)
+    Branch.where(name: 'Monclova').first.update(manager_id: User.last.id)
+    Department.where(name: 'Monclova').first.update(manager_id: User.last.id)
+
+
+
+# Branch.where(name: 'Alchichica').first.update(manager_id: User.last.id)
+    # Department.where(name: 'Alchichica').first.update(manager_id: User.last.id)
+    #
+    #
+    #
+    #
+    # Department.where(name: 'Tultitlán').first.update(manager_id: User.last.id)
+    # Department.where(name: 'Hasta Bandera').first.update(manager_id: User.last.id)
+    #
+
+
+    Company.create(name: 'AgroTrailers Planet', description: 'AgroTrailers Planet',
+                   address: 'Cambiar Direccion', phone: 'Cambiar Telefono',
+                   user_id: 1)
+
+  end
 end
