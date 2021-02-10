82605f4fe1ac0411ee4fc83b9e012d1f4ad62b3e
OrlandoRocks <mr.landodlp@gmail.com>
2021-02-10 13:03:08 -0700

    fix manager in branch ralation

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -4,10 +4,13 @@ class Branch < ApplicationRecord
   # belongs_to :state , :class_name => 'State' , foreign_key: 'state_id'
   belongs_to :city , :class_name => 'City' , foreign_key: 'city_id'
 
+  belongs_to :manager, class_name: 'User', foreign_key: 'manager_id', optional: true
+
   has_one :state, through: :city
 
+
   has_many :departments
-  has_many :managers, through: :departments
+
   has_many :items, through: :departments
 
   # delegate :name, to: :company, prefix: true, allow_nil: true
