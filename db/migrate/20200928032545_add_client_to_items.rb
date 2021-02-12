5ae3944ce87542bd6b645dcf5df585447b5d6724
OrlandoRocks <mr.landodlp@gmail.com>
2021-02-11 20:12:25 -0700

    add clients to items

--- db/migrate/20200928032545_add_client_to_items.rb
+++ db/migrate/20200928032545_add_client_to_items.rb
@@ -1,6 +1,5 @@
 class AddClientToItems < ActiveRecord::Migration[5.2]
   def change
-    add_reference :items, :client, index: true, foreign_key: true
     add_column :items, :advance_payment, :decimal
   end
 end
