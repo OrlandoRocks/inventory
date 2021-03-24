c548268ba42e2fcf19772f7577e848f1efed64a5
OrlandoRocks <mr.landodlp@gmail.com>
2021-03-24 16:50:17 -0700

    se corrigue selector de marca

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,17 +1,6 @@
 class Brand < ApplicationRecord
 
   has_many :trailer_category
-  has_many :items
-
   accepts_nested_attributes_for :trailer_category
 
-  before_destroy :check_for_items
-  private
-  def check_for_items
-    if items.count > 0
-      flash[:notice] = 'No es posible eliminar el estatus ya que existen artículos con este estatus.'
-      render 'brands_path'
-      return false
-    end
-  end
 end
