e04b1d45a0137c0becb0fa80bd16bc28fb0f3367
Ricky <ricky@mail.com>
2021-04-05 22:30:02 -0600

    api token thru param

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,22 +1,21 @@
 Warden::Strategies.add(:api_token) do
-  def valid?
-    #request.headers['HTTP_AUTHORIZATION'].start_with?('Bearear')
-    request.headers['Authorization'].present?
-    #params[:token].present?
-  end
-  def authenticate!
-    if auth_token && user_token
-      success!(user_token)
-    else
-      fail!('Not Authenticated') 
+    def valid?
+        # request.headers['Authorization'].present?
+        params[:token].present?
+    end
+    def authenticate!
+        if auth_token && user_token
+            success!(user_token)
+        else
+            fail!('Not Authenticated') 
+        end 
     end 
-  end 
-  private
-  def auth_token
-    @auth_token ||= JsonWebToken.decode(request.headers['Authorization'].split(' ').last)
-    # @auth_token ||=  JsonWebToken.decode(params[:token])
-  end 
-  def user_token
-    @user_token ||= User.find(auth_token[:user_id].to_i)
-  end
+    private
+    def auth_token
+        # @auth_token ||= JsonWebToken.decode(request.headers['Authorization'].split(' ').last)
+        @auth_token ||=  JsonWebToken.decode(params[:token])
+    end 
+    def user_token
+        @user_token ||= User.find(auth_token[:user_id].to_i)
+    end
 end
