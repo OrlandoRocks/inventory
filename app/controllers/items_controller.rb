cb8b7ccf37d36d09cb62255909fb99b4e242af20
Alejandro Rodriguez Gonzalez <alexrdgz03@gmail.com>
2021-08-09 14:47:00 -0600

    Se arregla la actualizacion de sale_price

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -291,6 +291,8 @@ class ItemsController < ApplicationController
 
     item_selled = @item.status_item_id != params[:status_item_id]
 
+    new_params['sale_price'] = new_params['sale_price'].to_s.gsub(/[$,]/,'').to_f
+
     respond_to do |format|
       if @item.update(new_params)
 
