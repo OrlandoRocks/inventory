c7d18c1094188067e0635a2cb15614ff3455d4e7
OrlandoRocks <mr.landodlp@gmail.com>
2021-09-25 10:54:37 -0600

    se arreglo rfc en el qr y las fechas en el pdf de la factura

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -170,7 +170,7 @@ class ItemsController < ApplicationController
     @number_string = @data['Total'].to_f.a_letras
 
     data = "?re=GRN030226P48&rr=#{@trailer.try(:client).try(:rfc)}&id=#{@data['Complemento']['TimbreFiscalDigital']['UUID']}"
-    data = "https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?id=#{@data['Complemento']['TimbreFiscalDigital']['UUID']}&re=#{@trailer.try(:client).try(:rfc)}&rr=GRN030226P48&tt=#{@data['Total']}&fe=#{@data['Complemento']['TimbreFiscalDigital']['SelloSAT'].last(8).to_s}"
+    data = "https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx?id=#{@data['Complemento']['TimbreFiscalDigital']['UUID']}&re=ATP200908A49&rr=#{@trailer.try(:client).try(:rfc)}&tt=#{@data['Total']}&fe=#{@data['Complemento']['TimbreFiscalDigital']['SelloSAT'].last(8).to_s}"
     qrcode = RQRCode::QRCode.new(data, :size => 20, :level => :h)
     @svg = qrcode.as_svg(
         offset: 0,
